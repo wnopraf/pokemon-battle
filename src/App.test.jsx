39912d7e0bfe44b4eac8ab477f54946705d16fac
wnopraf <e.bcncgroup.tid@telefonica.com>
2026-04-19 19:31:40 +0200

    initial commit

---
+++ src/App.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+import App from './App.jsx'
+
+test('renders Pokemon Battle heading', () => {
+  render(<App />)
+  const headingElement = screen.getByText(/Pokemon Battle/i)
+  expect(headingElement).toBeInTheDocument()
+})
