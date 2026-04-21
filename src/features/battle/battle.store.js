f7c45b040f99b35b545edd2641c215240a02f9df
wnopraf <e.bcncgroup.tid@telefonica.com>
2026-04-21 22:38:14 +0200

    add:battle store

---
+++ src/features/battle/battle.store.js
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+export const useBattleStore = create((set) => ({
+  battleResult: null,
+
+  setBattleResult: (result) =>
+    set({ battleResult: result }),
+
+  resetBattle: () =>
+    set({ battleResult: null }),
+}));
