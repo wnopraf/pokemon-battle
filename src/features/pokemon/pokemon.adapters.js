99e2c238baa055d80c587c7ba24805f620074a43
wnopraf <e.bcncgroup.tid@telefonica.com>
2026-04-21 22:35:30 +0200

    add:api mappers

---
+++ src/features/pokemon/pokemon.adapters.js
@@ -0,0 +1,26 @@
+export function mapPokemonListItem(apiItem) {
+  const id = apiItem.url.split("/").filter(Boolean).pop();
+
+  return {
+    id: Number(id),
+    name: apiItem.name,
+    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+  };
+}
+
+export function mapPokemon(apiPokemon) {
+  const getStat = (name) =>
+    apiPokemon.stats.find((s) => s.stat.name === name)?.base_stat ?? 0;
+
+  return {
+    id: apiPokemon.id,
+    name: apiPokemon.name,
+    image: apiPokemon.sprites?.front_default ?? "",
+
+    attack: getStat("attack"),
+    defense: getStat("defense"),
+    speed: getStat("speed"),
+
+    types: apiPokemon.types.map((t) => t.type.name),
+  };
+}
