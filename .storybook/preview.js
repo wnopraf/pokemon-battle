d777e4b80096f9dbb1cb9cadb131e7566c4f00c6
wnopraf <e.bcncgroup.tid@telefonica.com>
2026-04-25 23:05:16 +0200

    add:shacdn conf + storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+/** @type { import('@storybook/react-vite').Preview } */
+import "../src/index.css";
+
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
