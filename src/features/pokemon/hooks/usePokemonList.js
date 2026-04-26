a894ff084e1f7c0adb98df9996f073a171991f87
wnopraf <e.bcncgroup.tid@telefonica.com>
2026-04-26 08:50:41 +0200

    fix:use the right adapter

--- src/features/pokemon/hooks/usePokemonList.js
+++ src/features/pokemon/hooks/usePokemonList.js
@@ -1,15 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import * as pokemonApi from "../pokemon.api.js";
 import { pokemonKeys } from "../pokemon.keys.js";
-import { mapPokemonListItem } from "../pokemon.adapters.js";
+import { mapPokemonByType, mapPokemonListItem } from "../pokemon.adapters.js";
 
-export const usePokemonList = ({ type,limit = 20, offset = 0 }) => {
+export const usePokemonList = ({ type, limit = 20, offset = 0 }) => {
   return useQuery({
     queryKey: pokemonKeys.list({ type, limit, offset }),
     queryFn: async () => {
       if (type) {
         const response = await pokemonApi.getPokemonByType(type);
-        return response.map(mapPokemonListItem);
+        return mapPokemonByType(response);
       }
       const response = await pokemonApi.getPokemonList(limit, offset);
       return response.results.map(mapPokemonListItem);
