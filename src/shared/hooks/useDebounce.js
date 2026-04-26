7c8c553680ecd3f7a40d874a0b5b9c15597481ba
wnopraf <e.bcncgroup.tid@telefonica.com>
2026-04-26 11:00:04 +0200

    add:debounce hook

---
+++ src/shared/hooks/useDebounce.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export function useDebounce(value, delay = 300) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeoutId = window.setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      window.clearTimeout(timeoutId);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
