47e4dd0dae3388322b9932b6a9ed414de2f22088
wnopraf <e.bcncgroup.tid@telefonica.com>
2026-04-27 23:06:07 +0200

    remove:dead scaffold App.jsx and its test

--- src/App.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App.jsx";
-
-test("renders Pokemon Battle heading", () => {
-  render(<App />);
-  const headingElement = screen.getByText(/Pokemon Battle/i);
-  expect(headingElement).toBeInTheDocument();
-});
